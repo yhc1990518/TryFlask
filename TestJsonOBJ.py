6fe390554d300c3e55418b38c8b5a4e3cb0d43e5
eric <pro.anything@yahoo.com>
2019-08-01 19:15:57 +0800

    feat(*): 测试flask框架中对json的操作

---
+++ TestJsonOBJ.py
@@ -0,0 +1,8 @@
+from pyon import PyonObject
+
+
+class TestJsonOBJ:
+    def __init__(self):
+        self.id = 1
+        self.name = "test name"
+
--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask
+from flask import Flask, request
+from pyon import PyonObject
+import orjson
+
+
+import TestJsonOBJ
 
 app = Flask(__name__)
 
@@ -6,6 +11,20 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+'''
+测试json
+'''
+@app.route('/testjson', methods=['POST'])
+def testjson():
+    jsValue = request.get_json()
+    jsValue["id"] = 2
+    jsValue["name"] = "修改了"
+    return orjson.dumps(jsValue)
+
+
+
 
 
 if __name__ == '__main__':
